1718c37c2623c2a8ed4dc8625c2da8e1a44b111c
SZU-Bio-Lwp <113423865+SZU-Bio-Lwp@users.noreply.github.com>
2022-12-09 16:30:24 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+import pickle
 
 
 def load_data():
-    tr_d = pd.read_csv('', header=None)
-    te_d = pd.read_csv('', header=None)
+    tr_d = pickle.load(open('training_data.pkl', 'rb'))
+    te_d = pickle.load(open('testing_data.pkl', 'rb'))
 
     return np.array(tr_d), np.array(te_d)
 
@@ -13,15 +14,11 @@ def load_data_wrapper():
     tr_d, te_d = load_data()
 
     training_inputs = [np.reshape(x[:-1], (300, 1)) for x in tr_d]
-    training_inputs = np.array(training_inputs)
     training_results = [vectorized_result(y[-1]) for y in tr_d]
-    training_results = np.array(training_results)
     training_data = list(zip(training_inputs, training_results))
 
     test_inputs = [np.reshape(x[:-1], (300, 1)) for x in te_d]
-    test_inputs = np.array(test_inputs)
     test_results = [vectorized_result(y[-1]) for y in te_d]
-    test_results = np.array(test_results)
     test_data = list(zip(test_inputs, test_results))
 
     return training_data, test_data
