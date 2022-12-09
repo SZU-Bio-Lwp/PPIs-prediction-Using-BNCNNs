bff939f6065f46d79654214a37b15e087dcc7d39
SZU-Bio-Lwp <113423865+SZU-Bio-Lwp@users.noreply.github.com>
2022-12-09 16:44:21 +0800

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,32 +1,23 @@
-import pandas as pd
-import numpy as np
 from imblearn.over_sampling import BorderlineSMOTE
-import xlsxwriter
-
-
-def load_data():
-    original_data = pd.read_csv('', header=None)
-    return np.array(original_data)
+import pickle
 
 
 def generate_data():
-    or_d = load_data()
-
+    ori_d = pickle.load(open('ori_data.pkl', 'rb'))
+    smo_d = []
     smo = BorderlineSMOTE(kind="borderline-2")
 
-    x = [x[:-1] for x in or_d]
-    y = [y[-1] for y in or_d]
+    x = [x[:-1] for x in ori_d]
+    y = [y[-1] for y in ori_d]
     x_smo, y_smo = smo.fit_resample(x, y)
+    
+    for i in range(len(y_smo)):
+        smo_d.append(x_smo[i])
+        smo_d[i].append(y_smo[i])
 
-    workbook = xlsxwriter.Workbook('filename.csv')
-    worksheet = workbook.add_worksheet('blsmote_data')
-    for i in range(len(x_smo)):
-        j = 0
-        for x in x_smo[i]:
-            worksheet.write(i, j, x)
-            j += 1
-        worksheet.write(i, j, y_smo[i])
-    workbook.close()
+    result_file = "smo_data.pkl"
+    with open(result_file, "wb") as fp:
+        pickle.dump(smo_d, fp)
 
 
 if __name__ == "__main__":
